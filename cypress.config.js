5e00efc4df6d7ee1183cc1f0ce2d7cf2c8e2b1e9
Dora Charm <kovina.dora@gmail.com>
2024-03-30 12:31:40 +0500

    Connected to dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "stq85g",
   e2e: {
     baseUrl: "http://qamid.tmweb.ru",
     setupNodeEvents(on, config) {
